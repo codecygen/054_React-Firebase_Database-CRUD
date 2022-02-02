e6a4acd391a3cb1666b551be338cda84f5b2f535
gingercook <vahitaras@gmail.com>
2022-02-02 06:55:21 -0500

    environmental variables created for firestore database

--- README.md
+++ README.md
@@ -11,4 +11,10 @@
 
 3) Firebase Configuration File
 - "firebase-config.js" under "src" folder.
-- Copy the section
+- Copy the function "firebaseConfig" in "Settings" section of Firebase and paste into "firebase-config.js".
+- Create environmental variables ".env" file and put sensitive data inside it.
+
+4) Create "Firestore Database"
+- Click on "Firestore Database", then click on "Create Database" button.
+- Select "Start in production mode" then click on "Next" button.
+- Create a database with collections and data.
--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+import { db } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
+
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, 'users');
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+    };
+
+    getUsers();
+  }, []);
   return (
     <div className="App"></div>
   );
